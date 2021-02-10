97602ef2981951cceeb5ab4d192b9d5e58d40120
sameghosts <seanfeiner.brown@gmail.com>
2021-02-10 11:17:11 -0800

    fixed some styling of my card

--- src/SeanFeiner.js
+++ src/SeanFeiner.js
@@ -5,8 +5,8 @@ const SeanFeiner = () => {
 
   return(
     <div className="team-card">
-      <h1>Sean Feiner</h1>
       <img src="https://i.imgur.com/hbUgEd6.jpg" alt="pic of sean" />
+      <h1>Sean Feiner</h1>
       <p> I am a developer, writer, curator and artist with a passion for emergent art and music technologies and the cultures and platforms they generate. As a digital and performative creative my expertise is developed through research, dynamic and agile problem solving, and project devleopment and management, with expertise in the  academic, music, nightlife, and tech industries. 
  </p>
     </div>
