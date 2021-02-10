0e33556ae48e90ecb35f55a23c5a1a4fa9f8f5e8
sameghosts <seanfeiner.brown@gmail.com>
2021-02-10 11:06:55 -0800

    fixed my component

--- src/SeanFeiner.js
+++ src/SeanFeiner.js
@@ -3,13 +3,15 @@ import React from 'react';
 
 const SeanFeiner = () => {
 
-  render(
+  return(
     <div className="team-card">
       <h1>Sean Feiner</h1>
-      <img src="https://imgur.com/a/3LLGk0p" alt="pic of sean" />
+      <img src="https://i.imgur.com/hbUgEd6.jpg" alt="pic of sean" />
       <p> I am a developer, writer, curator and artist with a passion for emergent art and music technologies and the cultures and platforms they generate. As a digital and performative creative my expertise is developed through research, dynamic and agile problem solving, and project devleopment and management, with expertise in the  academic, music, nightlife, and tech industries. 
  </p>
     </div>
     )
 
-}
+};
+
+export default SeanFeiner;
