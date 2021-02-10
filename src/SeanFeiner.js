b8387c3ca877f5e8bcde5464ad11b151103cc880
austintip <austin.d.tipograph@gmail.com>
2021-02-10 14:15:52 -0500

    better photo

--- src/SeanFeiner.js
+++ src/SeanFeiner.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 const SeanFeiner = () => {
 
-  return(
+  return (
     <div className="team-card">
       <h1>Sean Feiner</h1>
       <img src="https://i.imgur.com/hbUgEd6.jpg" alt="pic of sean" />
-      <p> I am a developer, writer, curator and artist with a passion for emergent art and music technologies and the cultures and platforms they generate. As a digital and performative creative my expertise is developed through research, dynamic and agile problem solving, and project devleopment and management, with expertise in the  academic, music, nightlife, and tech industries. 
+      <p> I am a developer, writer, curator and artist with a passion for emergent art and music technologies and the cultures and platforms they generate. As a digital and performative creative my expertise is developed through research, dynamic and agile problem solving, and project devleopment and management, with expertise in the  academic, music, nightlife, and tech industries.
  </p>
     </div>
-    )
+  )
 
 };
 
