8ae54c2599cce4981e4a50631728a87452abb298
kys3923 <kys3923@gmail.com>
2021-02-10 13:51:35 -0500

    made my Young.js

---
+++ src/Young.js
@@ -0,0 +1,11 @@
+const Young = () => {
+    return (
+        <div className="team-card">
+            <h1>Hi I'm Young</h1>
+            <img src="/YKlogo.png" />
+            <p>I'm Young! It is my first time doing group work with git!</p>
+        </div>
+    )
+}
+
+export default Young
