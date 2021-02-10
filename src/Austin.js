5e6693acacfc8fd60931d333391b42d57f835889
austintip <austin.d.tipograph@gmail.com>
2021-02-10 13:53:52 -0500

    made some quick edits to p tag

--- src/Austin.js
+++ src/Austin.js
@@ -6,7 +6,7 @@ const Austin = () => {
             <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia-exp1.licdn.com%2Fdms%2Fimage%2FC5603AQF66WXjd2JAWw%2Fprofile-displayphoto-shrink_200_200%2F0%3Fe%3D1611792000%26v%3Dbeta%26t%3DjpanpTcMr8GMX-oDl-gjfbHlkyNsYGF8o-Jl8SfdzL4&imgrefurl=https%3A%2F%2Fwww.linkedin.com%2Fin%2Faustin-tipograph-522209137&tbnid=wNru2ia-SUlA9M&vet=12ahUKEwiR7cq9_d_uAhWFKlMKHV98AJgQMygAegQIARAo..i&docid=r7FSURIODcfjKM&w=200&h=200&q=austin%20tipograph&safe=off&ved=2ahUKEwiR7cq9_d_uAhWFKlMKHV98AJgQMygAegQIARAo"
             ></img>
             <h1>Austin Tipograph</h1>
-            <p>Austin loves few things as much as he likes Pokémon. There isn't really that much else you need to know. He is truly <strong>OBSESSED</strong>. You can see what his favorite games are <a href='https://www.tech21century.com/list-of-pokemon-games-in-order/' target='_blank' >here.</a> </p>
+            <p>Austin loves few things as much as he likes Pokémon. There isn't really that much else you need to know. He is truly <strong>OBSESSED</strong>. You can see what just a couple of his favorite games are <a href='https://www.tech21century.com/list-of-pokemon-games-in-order/' target='_blank' >here</a>.</p>
         </div>
     )
 }
